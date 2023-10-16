ab3a2d416a57d26b65626fcdb0db2ea2bdfb8c4b
Guevara <85024959+rakhaguevara@users.noreply.github.com>
2023-10-16 11:21:33 +0700

    first commit

---
+++ src/sections/CostumerReviews.jsx
@@ -0,0 +1,20 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+const CostumerReviews = () => {
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red">Costumers</span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem blanditiis eveniet quaerat quos exercitationem numquam, alias cum laboriosam?</p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard key={index} imgURL={review.imgURL} customerName={review.customerName} rating={review.rating} feedback={review.feedback} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CostumerReviews;
---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import CostumerReviews from "./CostumerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export { Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CostumerReviews, Subscribe, Footer };
